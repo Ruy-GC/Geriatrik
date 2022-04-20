201999e861d44a9ffb21ab6581438cf16e234ff1
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2022-04-19 20:36:33 -0500

    login/register first draft, no auth functionality

--- geriatrik-app/src/App.js
+++ geriatrik-app/src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, {Fragment} from 'react';
 import './App.css';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Home from './components/pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <Router>
+            <Fragment>
+                <Routes>
+                    <Route exact path='/' element = {<Home/>}/>
+                    <Route exact path='/login' element = {<Login/>}/>
+                    <Route exact path='/Register' element = {<Register/>}/>
+                </Routes>
+            </Fragment>
+            {/*<div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </header>
+            </div>*/}
+        </Router>
+        
   );
 }
 
---
+++ geriatrik-app/src/components/auth/Login.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from 'react'
+import { useNavigate  } from "react-router-dom"
+import login from '../layout/images/login.PNG'
+const Login = () => {
+
+    const navigate = useNavigate ();
+
+    const [user,setUser] = useState({
+        email: '',
+        password: ''
+    });
+
+    const {email,password} = user;
+
+    const onChange = e => setUser({...user, [e.target.name]:e.target.value});
+
+    const onSubmit = e => {
+        e.preventDefault();
+        navigate('/');
+        /*if(email === '' || password === ''){
+            //missing inputs
+        }else{
+            //login
+        }*/
+    }
+    
+    return (
+        <div className='grid-2'>
+            <div className='form-container'>
+                <h1 className='geriatrik'>GERIATRIK</h1>
+                <h2 className='auth-header'>Iniciar Sesión</h2>
+                <form onSubmit={onSubmit}>
+                    <input placeholder='Correo Electrónico' type='email' name = 'email' value={email} onChange = {onChange} required/>
+                    <input placeholder='Contraseña' type='password' name = 'password' value={password} onChange = {onChange} required/>
+                    <p style={{margin: '15px', cursor: 'pointer',textDecoration: 'underline'}} onClick = {() => navigate('/register', { replace: true })}>¿No tienes cuenta? Registrate</p>
+                    <input type="submit" value = "Iniciar Sesión"/>
+                </form>
+            </div>
+            <div>
+                <center>
+                    <img className='login-img'
+                        src={login}
+                        alt='Geriatra'
+                    />
+                </center>
+            </div>
+        </div>
+    )
+}
+
+export default Login
