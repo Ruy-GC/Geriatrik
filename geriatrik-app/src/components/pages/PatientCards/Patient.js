076fa5b404590b6ac8bc7367c271130c29f4a477
sant-gdc <83039994+sant-gdc@users.noreply.github.com>
2022-04-21 12:17:33 -0500

    Patient card update with justification and alignment

--- geriatrik-app/src/components/pages/PatientCards/Patient.js
+++ geriatrik-app/src/components/pages/PatientCards/Patient.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import OldMan from '../../layout/images/OldManPic.jpg'
+import './Patient.css' 
 
 const Patient =() =>{
     return(
     <div className='Card'>
-        <PatientInfo /> 
+        <PatientInfo 
+        name = "Adrian Becerra"
+        age = "89"
+        sex = "Hombre"/> 
     </div>
     )
 }
 
-const PatientInfo =() =>{
+const PatientInfo =(props) =>{
     return(
     <div className='CardInfo'>
         <div className='CardBody'>
-            <img src={OldMan}/>
-            <h1 className='PatientName'>Adrian Becerra</h1>
-            <h2 className='PatientAge'>89</h2>
-            <h3 className='PatientSex'>Hombre</h3>
+            <img className='PatientImg' src={OldMan}/>
+            <h1 className='PatientName'>{props.name}</h1>
+            <h2 className='PatientAge'>{props.age}</h2>
+            <h3 className='PatientSex'>{props.sex}</h3>
         </div>
     </div>
     )
