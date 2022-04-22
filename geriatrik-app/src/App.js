01ef8c83daa64edfde30533c7172b0ab0f09835d
AdrianBecerra411 <71723297+AdrianBecerra411@users.noreply.github.com>
2022-04-22 10:57:20 -0500

    Test_button components
    Boton de Evaluaciones

--- geriatrik-app/src/App.js
+++ geriatrik-app/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React, {Fragment} from 'react';
-import './App.css';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Home from './components/pages/Home';
-import Patient from './components/pages/PatientCards/Patient';
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { Fragment } from "react";
+import "./App.css";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Home from "./components/pages/Home";
+import Reports from "./components/pages/Reports";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
--- geriatrik-app/src/components/pages/Reports.js
+++ geriatrik-app/src/components/pages/Reports.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import Navbar from '../Navbar'
+import TestButton from '../TestButton'
+import "./styles.css"
+
+const HomeLink = "/"
+
 const Reports = () => {
   return (
-    <div>Home
+    <div>
 <Navbar />
+
+  <TestButton onClick={() => {console.log("You Clicked on Me!")}}
+  type="button"
+  buttonStyle="btn--secondary--solid"
+  buttonSize="btn--large"
+  link = { HomeLink }
+  >MOCA</TestButton>
+
+<TestButton onClick={() => {console.log("You Clicked on Me!")}}
+  type="button"
+  buttonStyle="btn--primary--solid"
+  buttonSize="btn--large"
+  link = { HomeLink }
+  >Actividades Instrumentales</TestButton>
+
+<TestButton onClick={() => {console.log("You Clicked on Me!")}}
+  type="button"
+  buttonStyle="btn--primary--solid"
+  buttonSize="btn--large"
+  link = { HomeLink }
+  >Actividades Basicas</TestButton>
+  
+
 </div>
   )
 }
 
-export default Home
+export default Reports
