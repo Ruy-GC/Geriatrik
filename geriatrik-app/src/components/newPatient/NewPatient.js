c831ca2c335b10ea71d3c509469fad88ad7fec25
fernandolpz21 <86685165+fernandolpz-A01639715@users.noreply.github.com>
2022-04-24 14:18:53 -0500

    Update form

--- geriatrik-app/src/components/newPatient/NewPatient.js
+++ geriatrik-app/src/components/newPatient/NewPatient.js
@@ -10,20 +10,21 @@ const NewPatient = () => {
 
     //user register fields
     const [user,setUser] = useState({
+        ID: '',
         name: '',
-        firstName: '',
+        lastName: '',
+        motherLastName: '',
         birthday: '',
         gender: '',
-        age: '',
-        emergencyContact: '',
         scholarity: '',
-        memoryComplaint: '',
         disabilities: '',
+        memoryComplaint: '',
         severeHearingLoss: '',
-        image: ''
+        emergencyContact: '',
+        profileImage: ''
     });
 
-    const {name,firstName, birthday, gender, age, emergencyContact, scholarity, memoryComplaint, disabilities, severeHearingLoss, image} = user;
+    const {ID, name, lastName, motherLastName, birthday, gender, scholarity, disabilities, memoryComplaint, severeHearingLoss, emergencyContact, image} = user;
 
     //copies user data and changes the target of the onchange
     const onChange = e => setUser({...user, [e.target.name]:e.target.value});
@@ -31,7 +32,7 @@ const NewPatient = () => {
     const onSubmit = e => {
         e.preventDefault();
 
-        if(name === '' || firstName === '' || gender === '' || age === '' || emergencyContact === '' || scholarity === '' || memoryComplaint === '' || disabilities === '' || severeHearingLoss === '' || birthday === ''){
+        if(name === '' || lastName === '' || motherLastName === '' || birthday === '' || gender === '' || scholarity === '' || disabilities === '' || memoryComplaint === '' || severeHearingLoss === '' || emergencyContact === ''){
             //missing info
         }else{
             //register
@@ -39,6 +40,7 @@ const NewPatient = () => {
         }
     }
     
+    //El onfocus="(this.type='date')" no sirve
     return (
         <div className='grid-2'>
             <div className='form-container'>
@@ -46,19 +48,23 @@ const NewPatient = () => {
                 <h2 className='auth-header'>Add New Patient</h2>
                 <form onSubmit={onSubmit}>
                     <input placeholder='Nombre' type='text' name = 'name' value={name} onChange = {onChange} required/>
-                    <input placeholder='Apellido' type='text' name = 'firstName' value={firstName} onChange = {onChange} required/>
-                    <input placeholder='Fecha Nacimiento' type='date' name = 'birthday' value={birthday} onChange = {onChange} required/>
-                    <input placeholder='Sexo' type='text' name = 'gender' value={gender} onChange = {onChange} required/>
-                    <input placeholder='Edad' type='number' name = 'age' value={age} onChange = {onChange} required/>
-                    <input placeholder='Contacto de Emergencia' type='number' name = 'emergencyContact' value={emergencyContact} onChange = {onChange} required/>
-                    <input id = "escolaridad" type='checkbox' name = 'scholarity' value={scholarity} onChange = {onChange}required/>
-                    <input placeholder='Queja de memoria' type='text' name = 'memoryComplaint' value={memoryComplaint} onChange = {onChange} required/>
+                    <input placeholder='Apellido Paterno' type='text' name = 'lastName' value={lastName} onChange = {onChange} required/>
+                    <input placeholder='Apellido Materno' type='text' name = 'motherLastName' value={motherLastName} onChange = {onChange} required/>
+                    <label for="fe">Fecha de Nacimiento</label>
+                    <input type='date' name = 'birthday' value={birthday} onChange = {onChange} required/>
+                    <input placeholder='Sexo (H/M)' type='text' name = 'gender' value={gender} onChange = {onChange} required/>
+                    <label for="escolaridad">Escolaridad</label>
+                    <input id = "escolaridad" type='checkbox' name = 'scholarity' value={scholarity} onChange = {onChange}/>
                     <input placeholder='Discapacidades' type='text' name = 'disabilities' value={disabilities} onChange = {onChange} required/>
-                    <input placeholder='Hipoacusia severa' type='checkbox' name = 'severeHearingLoss' value={severeHearingLoss} onChange = {onChange} required/>
-                    <input placeholder='Imagen de Perfil' type='image' name = 'image' value={image} onChange = {onChange}/>
-                    <input type="submit" value = "Agregar Paciente"/>
+                    <label>Queja de memoria</label>
+                    <input placeholder='Queja de memoria' type='checkbox' name = 'memoryComplaint' value={memoryComplaint} onChange = {onChange} />
+                    <label>Hipoacusia severa</label>
+                    <input placeholder='Hipoacusia severa' type='checkbox' name = 'severeHearingLoss' value={severeHearingLoss} onChange = {onChange} />
+                    <input placeholder='Contacto de Emergencia' type='number' name = 'emergencyContact' value={emergencyContact} onChange = {onChange} required/>
+                    <label for="files">Foto de perfil</label>
+                    <input type='file' id = "files" value={image} accept="image/png, image/jpeg" onChange = {onChange}/>
+                    <input type="submit" value = "Agregar Paciente" formTarget='_blank'/>
                     <button className='btn-back' onClick={() => navigate('/home')}> Atrás</button>
-                    
                     
                 </form>
             </div>
