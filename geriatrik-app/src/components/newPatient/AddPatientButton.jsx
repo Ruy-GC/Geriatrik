e410a334b4e4d2667733eebf6019bf6722724dc7
fernandolpz21 <86685165+fernandolpz-A01639715@users.noreply.github.com>
2022-04-28 14:03:08 -0500

    Modal Implementation

--- geriatrik-app/src/components/newPatient/AddPatientButton.jsx
+++ geriatrik-app/src/components/newPatient/AddPatientButton.jsx
@@ -1,12 +1,119 @@
 import { useNavigate  } from "react-router-dom"
+import React, {useState} from 'react'
+import './newPatient.css'
+
 
 const AddPatientButton = () => {
     const navigate = useNavigate ();
-    return(
-        <button className='addPatientButton' onClick={() => navigate('/NewPatient')}>.</button>
-    )
-    //Implementar un modal y hacerlo sobre home
+    /*  ---  MODAL FORM --- */
+    
+    //user register fields
+    const [user,setUser] = useState({
+        name: '',
+        lastName: '',
+        motherLastName: '',
+        birthday: '',
+        gender: '',
+        scholarity: '',
+        disabilities: '',
+        memoryComplaint: '',
+        severeHearingLoss: '',
+        emergencyContact: '',
+        profileImage: ''
+    });
 
+    const {name, lastName, motherLastName, birthday, gender, scholarity, disabilities, memoryComplaint, severeHearingLoss, emergencyContact, image} = user;
+
+    //copies user data and changes the target of the onchange
+    const onChange = e => setUser({...user, [e.target.name]:e.target.value});
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        if(name === '' || lastName === '' || motherLastName === '' || birthday === '' || gender === '' || scholarity === '' || disabilities === '' || memoryComplaint === '' || severeHearingLoss === '' || emergencyContact === ''){
+            //missing info
+        }else{
+            //register
+            navigate('/home');
+        }
+    }
+    
+    /*  ---  MODAL ACTIONS --- */
+    let modal = document.querySelectorAll(".modal")[0];
+    let modalContainer = document.querySelectorAll(".modal-container")[0];
+
+    const open = e => {
+        e.preventDefault();
+        modalContainer.style.opacity = "1";
+        modalContainer.style.visibility ="visible";
+        modal.classList.toggle("modal-close");
+    }
+    const close = () => {
+        modal.classList.toggle("modal-close");
+        setTimeout(function(){
+            modalContainer.style.opacity = "0";
+            modalContainer.style.visibility = "hidden";
+        }, 600)//vamos a darle 1s para ejecutar la función
+    }
+    
+    window.addEventListener("click", function(e){
+        console.log(e.target);
+        if(e.target == modalContainer){
+            close();
+        }
+    })
+
+    return(
+        <section>
+            <a href="#" class='addPatientButton' onClick={open}>.</a>
+            <div class = "modal-container">
+                <div class = "modal modal-close">
+                    <a class = "close" onClick={close}>X</a>
+                    <div class = "modal-text">
+                        <h2 className='auth-header'>Add New Patient</h2>
+                        <form onSubmit={onSubmit}>
+                            <div className='grid-2'>
+                                <div>
+                                    <input placeholder='Nombre' type='text' name = 'name' value={name} onChange = {onChange} required/>
+                                    <input placeholder='Apellido Paterno' type='text' name = 'lastName' value={lastName} onChange = {onChange} required/>
+                                    <input placeholder='Apellido Materno' type='text' name = 'motherLastName' value={motherLastName} onChange = {onChange} required/>
+                                    <input type='date' name = 'birthday' value={birthday} onChange = {onChange} required/>
+                                    <select  id="sexos" name='sex' onChange = {onChange}>
+                                        <option value="Masculino">Masculino</option>
+                                        <option value="Femenino">Femenino</option>
+                                    </select >
+                                    <input placeholder='Escolaridad' type='text' name = 'scholarity' value={scholarity} onChange = {onChange} required/>
+                                </div>
+                                <div>
+                                    <input placeholder='Discapacidades' type='text' name = 'disabilities' value={disabilities} onChange = {onChange} required/>
+                                    <div class ='toggle-div'>
+                                        <label class ='toggle-label'>Queja de Memoria</label>
+                                        <label class="toggle">
+                                            <input type='checkbox' name = 'memoryComplaint' value={memoryComplaint} onChange = {onChange} />
+                                            <span class = "slider"></span>
+                                            <span class ="labels" data-on="SI" data-off="NO"></span>
+                                        </label>
+                                    </div>
+                                    <div class ='toggle-div'>    
+                                        <label class ='toggle-label'>Hipoacusia severa</label>
+                                        <label class="toggle">
+                                        <input placeholder='Hipoacusia severa' type='checkbox' name = 'severeHearingLoss' value={severeHearingLoss} onChange = {onChange} />
+                                            <span class = "slider"></span>
+                                            <span class ="labels" data-on="SI" data-off="NO"></span>
+                                        </label>
+                                    </div>
+                                
+                                    <input placeholder='Contacto de Emergencia' type='number' name = 'emergencyContact' value={emergencyContact} onChange = {onChange} required/>
+                                    <input placeholder='URL Foto de perfil' type = 'text' id = "files" value={image} accept="image/png, image/jpeg" onChange = {onChange}/>
+                                </div>
+                            </div>
+                            <input type="submit" value = "Agregar Paciente" formTarget='_blank'/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    )
 }
 export default AddPatientButton
-
--- geriatrik-app/src/components/newPatient/NewPatient.js
+++ geriatrik-app/src/components/newPatient/NewPatient.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import { useNavigate  } from "react-router-dom"
-import login from '../layout/images/login.PNG'
+
 
 const NewPatient = () => {
     
@@ -42,36 +42,34 @@ const NewPatient = () => {
     return (
         <div className='grid-2'>
             <div className='form-container'>
-                <h1 className='geriatrik'>GERIATRIK</h1>
                 <h2 className='auth-header'>Add New Patient</h2>
                 <form onSubmit={onSubmit}>
-                    <input placeholder='Nombre' type='text' name = 'name' value={name} onChange = {onChange} required/>
-                    <input placeholder='Apellido Paterno' type='text' name = 'lastName' value={lastName} onChange = {onChange} required/>
-                    <input placeholder='Apellido Materno' type='text' name = 'motherLastName' value={motherLastName} onChange = {onChange} required/>
-                    <label for="fe">Fecha de Nacimiento</label>
-                    <input type='date' name = 'birthday' value={birthday} onChange = {onChange} required/>
-                    <input placeholder='Sexo (H/M)' type='text' name = 'gender' value={gender} onChange = {onChange} required/>
-                    <input placeholder='Escolaridad' type='checkbox' name = 'scholarity' value={scholarity} onChange = {onChange} required/>
-                    <input placeholder='Discapacidades' type='text' name = 'disabilities' value={disabilities} onChange = {onChange} required/>
-                    <label>Queja de memoria</label>
-                    <input placeholder='Queja de memoria' type='checkbox' name = 'memoryComplaint' value={memoryComplaint} onChange = {onChange} />
-                    <label>Hipoacusia severa</label>
-                    <input placeholder='Hipoacusia severa' type='checkbox' name = 'severeHearingLoss' value={severeHearingLoss} onChange = {onChange} />
-                    <input placeholder='Contacto de Emergencia' type='number' name = 'emergencyContact' value={emergencyContact} onChange = {onChange} required/>
-                    <label for="files">Foto de perfil</label>
-                    <input type='file' id = "files" value={image} accept="image/png, image/jpeg" onChange = {onChange}/>
+                    <div className='grid-2'>
+                        <div>
+                            <input placeholder='Nombre' type='text' name = 'name' value={name} onChange = {onChange} required/>
+                            <input placeholder='Apellido Paterno' type='text' name = 'lastName' value={lastName} onChange = {onChange} required/>
+                            <input placeholder='Apellido Materno' type='text' name = 'motherLastName' value={motherLastName} onChange = {onChange} required/>
+                            <label for="fe">Fecha de Nacimiento</label>
+                            <input type='date' name = 'birthday' value={birthday} onChange = {onChange} required/>
+                            <select  id="sexos" name='sex' onChange = {onChange}>
+                                <option value="Masculino">Masculino</option>
+                                <option value="Femenino">Femenino</option>
+                            </select >
+                        </div>
+                        <div>
+                            <input placeholder='Escolaridad' type='text' name = 'scholarity' value={scholarity} onChange = {onChange} required/>
+                            <input placeholder='Discapacidades' type='text' name = 'disabilities' value={disabilities} onChange = {onChange} required/>
+                            <label>Queja de memoria</label>
+                            <input placeholder='Queja de memoria' type='checkbox' name = 'memoryComplaint' value={memoryComplaint} onChange = {onChange} />
+                            <label>Hipoacusia severa</label>
+                            <input placeholder='Hipoacusia severa' type='checkbox' name = 'severeHearingLoss' value={severeHearingLoss} onChange = {onChange} />
+                            <input placeholder='Contacto de Emergencia' type='number' name = 'emergencyContact' value={emergencyContact} onChange = {onChange} required/>
+                            <input placeholder='URL Foto de perfil' type = 'text' id = "files" value={image} accept="image/png, image/jpeg" onChange = {onChange}/>
+                        </div>
+                    </div>
                     <input type="submit" value = "Agregar Paciente" formTarget='_blank'/>
-                    <button className='btn-back' onClick={() => navigate('/home')}> Atrás</button>
-                    
                 </form>
-            </div>
-            <div>
-                <center>
-                    <img className='login-img'
-                        src={login}
-                        alt='Geriatra'
-                    />
-                </center>
+                <button className='btn-back' onClick={() => navigate('/home')}> Atrás</button>
             </div>
         </div>
     )
