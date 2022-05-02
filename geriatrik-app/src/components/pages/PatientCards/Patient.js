d374a8431f835f18425371715d799d7caa4ea61a
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2022-05-02 18:03:43 -0500

    Update Patient.js

--- geriatrik-app/src/components/pages/PatientCards/Patient.js
+++ geriatrik-app/src/components/pages/PatientCards/Patient.js
@@ -1,36 +1,36 @@
-import React, { useEffect } from 'react'
-import {useState} from 'react'
-import OldMan from '../../layout/images/OldManPic.jpg'
-import './Patient.css' 
-import  TestButton  from '../../TestButton'
-import TestGraph from '../../graph/TestGraph'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import OldMan from "../../layout/images/OldManPic.jpg";
+import "./Patient.css";
+import TestButton from "../../TestButton";
+import TestGraph from "../../graph/TestGraph";
 import { Bar } from "react-chartjs-2";
-import ListCardTest from './ListCardTest'
+import ListCardTest from "./ListCardTest";
 import { useParams } from "react-router-dom";
 
 const HomeLink = "/";
 
 const Details = (props) => {
   const [datos, setData] = React.useState(null);
-  const pacienteID = 1;
-  let valuesArray; 
+  let valuesArray;
 
   React.useEffect(() => {
-    fetch(`/tamizaje/${encodeURIComponent(pacienteID)}"`, {
+    fetch(`/tamizaje/${encodeURIComponent(props.patientID)}"`, {
       method: "GET",
       headers: { "Content-Type": "application/json" },
     })
       .then((res) => res.json())
       .then((data) => setData(data.message));
-    }, []);
+  }, []);
   // console.log(datos);
 
-  if(datos) {
+  if (datos) {
     datos.map((currentTest) => {
       valuesArray = JSON.parse(currentTest.respuestasJSON);
+      console.log(valuesArray);
       // console.log(valuesArray)
       // console.log(valuesArray.resultados.abstraccion)
-    })
+    });
   }
 
   // if (valuesArray) {
@@ -38,115 +38,6 @@ const Details = (props) => {
   //     console.log(dato)
   //   })
   // }
-
-const examenes = ([
-    {
-        ExamenId: 1,
-        examen: "Moca",
-        resultados: "90/100",
-        fecha: "22-08-2021",
-        notas: "Sin notas",
-    },
-    {
-        ExamenId: 2,
-        examen: "Act. Basicas",
-        resultados: "90/100",
-        fecha: "22-08-2021",
-        notas: "Sin notas",
-    },
-    {
-        ExamenId: 3,
-        examen: "Act. Instrumentales",
-        resultados: "90/100",
-        fecha: "22-08-2021",
-        notas: "Sin notas",
-    },
-]);
-
-const getEdad = (dateString) => {
-    let hoy = new Date()
-    let fechaNacimiento = new Date(dateString)
-    let edad = hoy.getFullYear() - fechaNacimiento.getFullYear()
-    let diferenciaMeses = hoy.getMonth() - fechaNacimiento.getMonth()
-    if (
-      diferenciaMeses < 0 ||
-      (diferenciaMeses === 0 && hoy.getDate() < fechaNacimiento.getDate())
-    ) {
-      edad--
-    }
-    return edad
-}
-
-/*
-    El paciente espera recibir los siguientes parametros 
-    para rellenar su informacion en el perfil
-    pacienteID: number,
-    nombre: string,
-    apellidoP: string,
-    apellidoM: string,
-    fechaNac: string,
-    sexo: string,
-    escolaridad: string,
-    discapacidades: string,
-    quejaMemoria: Boolean,
-    hipoacusia_severa: Boolean,
-    contactoEmergencia: number,
-    imagenPerfil: string,
-*/
-const Patient =() =>{
-    const { id } = useParams();
-    const [datos, setData] = React.useState(null);
-    React.useEffect(() => {
-        fetch('/patient/'+id)
-          .then((res) => res.json())
-          .then((data) => setData(data.message));
-    }, [id]);
-    return(
-        !datos ? <h1>Loading</h1>:
-        <div className='Card'>
-            <PatientInfo 
-                image = {datos[0].imagenPerfil}
-                name = {datos[0].nombre}
-                age = {getEdad(datos[0].fechaNac)}
-                sex = {datos[0].sexo}
-            /> 
-            <PatientRegistry
-                emergencyC = {datos[0].contactoEmergencia}
-                school = {datos[0].escolaridad}
-                memory = {datos[0].quejaMemoria ? "Si" : "No"}
-                difficulties = {datos[0].discapacidades}
-                hipoacusia = {datos[0].hipoacusia_severa ? "Si" : "No"} 
-            />
-            <Details
-                /*fecha1 = "22-09-2020"
-                fecha2 = "22-03-2021"
-                fecha3 = "22-09-2021"
-                fecha4 = "22-03-2022"
-                data1 = "28"
-                data2 = "27"
-                data3 = "27"
-                data4 = "25"*/
-                />
-        </div>  
-    )
-}
-
-const PatientInfo =(props) =>{
-    return(
-        <div className='CardInfo'>
-            <div className='CardBody'>
-                <img className='PatientImg' src={props.image} alt='PatientPP'/>
-                <br></br><br></br>
-                <h1 className='PatientName'>{props.name}</h1>
-                <br></br>
-                <h2 className='PatientAge'>Edad: {props.age}</h2>
-                <br></br>
-                <h2 className='PatientSex'>Sexo: {props.sex}</h2>
-                <br></br><br></br>
-            </div>
-        </div>
-    )
-}
 
   return (
     <div className="CardDetails">
@@ -190,26 +81,30 @@ const PatientInfo =(props) =>{
             <thead>
               <tr>
                 <th></th>
-                <th>Examenes</th>
+                <th>Examen</th>
                 <th>Resultados</th>
                 <th>Fecha</th>
                 <th>Notas</th>
               </tr>
             </thead>
             <tbody>
-              { datos ? datos.map((currentTest) => {
-                return (
-                  <ListCardTest
-                    testObj={currentTest}
-                    key={currentTest.tamizajeID}
-                    examen={currentTest.tipo}
-                    puntos={currentTest.puntos}
-                    fecha={currentTest.fecha}
-                    notas={currentTest.notas}
-                    respuestasJSON = {JSON.parse(currentTest.respuestasJSON)}
+              {datos ? (
+                datos.map((currentTest) => {
+                  return (
+                    <ListCardTest
+                      testObj={currentTest}
+                      key={currentTest.tamizajeID}
+                      examen={currentTest.tipo}
+                      puntos={currentTest.puntos}
+                      fecha={currentTest.fecha}
+                      notas={currentTest.notas}
+                      respuestasJSON={JSON.parse(currentTest.respuestasJSON)}
                     />
-                    );
-                  }) : <h1>Faltan datos</h1>}
+                  );
+                })
+              ) : (
+                <h1>Faltan datos</h1>
+              )}
             </tbody>
           </table>
         }
@@ -219,24 +114,62 @@ const PatientInfo =(props) =>{
   );
 };
 
+const getEdad = (dateString) => {
+  let hoy = new Date();
+  let fechaNacimiento = new Date(dateString);
+  let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+  let diferenciaMeses = hoy.getMonth() - fechaNacimiento.getMonth();
+  if (
+    diferenciaMeses < 0 ||
+    (diferenciaMeses === 0 && hoy.getDate() < fechaNacimiento.getDate())
+  ) {
+    edad--;
+  }
+  return edad;
+};
+
+/*
+    El paciente espera recibir los siguientes parametros 
+    para rellenar su informacion en el perfil
+    pacienteID: number,
+    nombre: string,
+    apellidoP: string,
+    apellidoM: string,
+    fechaNac: string,
+    sexo: string,
+    escolaridad: string,
+    discapacidades: string,
+    quejaMemoria: Boolean,
+    hipoacusia_severa: Boolean,
+    contactoEmergencia: number,
+    imagenPerfil: string,
+*/
 const Patient = () => {
-  return (
+  const { id } = useParams();
+  const [datos, setData] = React.useState(null);
+  React.useEffect(() => {
+    fetch("/patient/" + id)
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, [id]);
+  return !datos ? (
+    <h1>Loading</h1>
+  ) : (
     <div className="Card">
       <PatientInfo
-        image={OldMan}
-        name="Adrian Becerra"
-        age="89"
-        sex="Masculino"
+        image={datos[0].imagenPerfil}
+        name={datos[0].nombre}
+        age={getEdad(datos[0].fechaNac)}
+        sex={datos[0].sexo}
       />
       <PatientRegistry
-        emergencyC="333-222-2200"
-        school="Secundaria"
-        memory="Si"
-        difficulties="No"
-        hipoacusia="No"
+        emergencyC={datos[0].contactoEmergencia}
+        school={datos[0].escolaridad}
+        memory={datos[0].quejaMemoria ? "Si" : "No"}
+        difficulties={datos[0].discapacidades}
+        hipoacusia={datos[0].hipoacusia_severa ? "Si" : "No"}
       />
-      <Details 
-      />
+      <Details patientID={id} />
     </div>
   );
 };
